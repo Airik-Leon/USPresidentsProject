5270bcb4597beab1d5beb9ce915db9c2710c7a98
Airik Leon <airik.leon@gmail.com>
2017-11-03 16:49:57 -0600

    DAOImpl added

---
+++ src/com/skilldistillery/data/USPresidentDAOImpl.java
@@ -0,0 +1,27 @@
+package com.skilldistillery.data;
+
+import java.util.List;
+
+public class USPresidentDAOImpl implements PresidentDAO {
+
+	@Override
+	public List<President> filter() {
+		return null;
+	}
+
+	@Override
+	public President getPresident(int termNumber) {
+		return null;
+	}
+
+	@Override
+	public List<President> getPresidentsByParty(String party) {
+		return null;
+	}
+
+	@Override
+	public List<President> getListPresidents() {
+		return null;
+	}
+
+}
