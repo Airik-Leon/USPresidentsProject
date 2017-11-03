ce0b4560bbae889548085a8883bf30023adcf830
David Stumpe <dstumpe264@gmail.com>
2017-11-03 17:14:45 -0600

    made changes to servlet, added a dao object, changed the doGet and doPost began adding session stuff

--- src/com/skilldistillery/web/USPresidentServlet.java
+++ src/com/skilldistillery/web/USPresidentServlet.java
@@ -6,6 +6,10 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import com.skilldistillery.data.PresidentDAO;
+import com.skilldistillery.data.USPresidentDAOImpl;
 
 /**
  * Servlet implementation class USPresidentServlet
@@ -13,28 +17,31 @@ import javax.servlet.http.HttpServletResponse;
 //@WebServlet("/USPresidentServlet")
 public class USPresidentServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
+	
+	private PresidentDAO dao;
+	
+	
 
-    /**
-     * Default constructor. 
-     */
-    public USPresidentServlet() {
-        // TODO Auto-generated constructor stub
+    @Override
+	public void init() throws ServletException {
+    	dao = new USPresidentDAOImpl();
+	}
+
+	public USPresidentServlet() {
     }
 
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		HttpSession session = request.getSession();
 	}
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		doPost(request, response);
 	}
 
 }
